edc6a921eb620c0a62084c92b3377cfe3df8c5c5
Netanel Friedman <netanel.friedman6@gmail.com>
2024-04-04 02:14:28 +0300

    Menu handler fields + methods

--- trivia/src/handler/menu.rs
+++ trivia/src/handler/menu.rs
@@ -1,8 +1,48 @@
+use std::sync::Arc;
+
 use crate::messages::{RequestInfo, RequestResult};
+use crate::managers::login::LoggedUser;
 
-use super::Handler;
+use super::{Handler, RequestHandlerFactory};
 
-pub struct MenuRequestHandler;
+pub struct MenuRequestHandler {
+    user: LoggedUser,
+    factory: Arc<RequestHandlerFactory>,
+}
+
+impl MenuRequestHandler {
+    pub fn new(factory: Arc<RequestHandlerFactory>, user: LoggedUser) -> Self {
+        Self { factory, user }
+    }
+
+    fn signout(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn get_rooms(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn get_players_in_rooms(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn get_personal_stats(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn get_high_scores(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn join_room(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+
+    fn create_room(request_info: RequestInfo) -> anyhow::Result<RequestResult> {
+        todo!()
+    }
+}
 
 impl Handler for MenuRequestHandler {
     fn relevant(&self, request_info: &RequestInfo) -> bool {
