42576caec51a10ef205a1fc652d8f6b5a6a77547
Netanel Friedman <netanel.friedman6@gmail.com>
2024-04-05 13:14:34 +0300

    Move all the consts into one file

---
+++ client/src/consts.rs
@@ -0,0 +1,17 @@
+use iced::{Color, Length};
+
+pub const TITLE_SIZE: u16 = 70;
+pub const SUBTITLE_SIZE: u16 = 17;
+pub const ALREADY_HAVE_AN_ACCOUNT_SIZE: u16 = 12;
+pub const BUTTONS_PADDING: u16 = 2;
+pub const INPUT_FIELDS_SPACING: u16 = 10;
+pub const INPUT_FIELDS_PADDING: u16 = 10;
+pub const INPUT_FIELDS_MAX_WIDTH: u16 = 300;
+pub const TITLES_SPACING: u16 = 10;
+pub const TITLES_PADDING: u16 = 10;
+
+pub const SUBTITLE_COLOR: Color = Color::from_rgba(0.7, 0.7, 0.7, 0.7);
+pub const ALREADY_HAVE_AN_ACCOUNT_COLOR: Color = Color::from_rgb(0.2, 0.4, 0.7);
+
+pub const TITLES_PORTION: Length = Length::FillPortion(1);
+pub const INPUT_FIELDS_PORTION: Length = Length::FillPortion(2);
