e7a55f596043479be682c3a3038f44caddf48b0e
Netanel Friedman <netanel.friedman6@gmail.com>
2024-04-23 15:50:59 +0300

    Use actual DB file instead of using in-memory DB

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,6 @@ use trivia::db::SqliteDatabase;
 use trivia::server::TriviaServer;
 
 fn main() {
-    let db = SqliteDatabase::connect(":memory:").unwrap();
+    let db = SqliteDatabase::connect("trivia-db.sqlite").unwrap();
     TriviaServer::build("127.0.0.1:6969", db).unwrap().run();
 }
