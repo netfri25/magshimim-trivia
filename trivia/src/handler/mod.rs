274dbae42c8ff21001f2c0b89d7126e25154ca55
Netanel Friedman <netanel.friedman6@gmail.com>
2024-05-04 21:25:57 +0300

    Add an explanation for the 'db lifetime in Handler

--- trivia/src/handler/mod.rs
+++ trivia/src/handler/mod.rs
@@ -18,6 +18,12 @@ use crate::messages::{RequestInfo, RequestResult};
 
 pub use db::Error;
 
+// some handlers hold a reference to the db
+// for that reason I need to "save" the db lifetime in the trait and the RequestResult
+// handlers may also hold the RequestHandlerFactory, and therefore they also need a 'factory
+// lifetime, but because the factory lives as long as db ('factory: 'db) therefore I don't need to
+// hold a lifetime to it, because if it can live long enough to hold a reference to the db then it
+// surely lives long enough to hold a reference to the factory
 pub trait Handler<'db> {
     fn relevant(&self, request_info: &RequestInfo) -> bool;
     fn handle(&mut self, request_info: RequestInfo) -> Result<RequestResult<'db>, Error>;
