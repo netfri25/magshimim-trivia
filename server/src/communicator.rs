d651acd650db8f8f9bbd096a4ce50e0530a7884b
Netanel Friedman <netanel.friedman6@gmail.com>
2024-05-10 13:27:39 +0300

    Disconnect the client on an error

--- server/src/communicator.rs
+++ server/src/communicator.rs
@@ -4,7 +4,7 @@ use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 use trivia::db::Database;
 use trivia::handler::{self, Handler, RequestHandlerFactory};
-use trivia::messages::{self, Request, RequestInfo, RequestResult};
+use trivia::messages::{self, Request, RequestInfo, RequestResult, Response};
 use trivia::username::Username;
 
 use crate::defer::Defer;
@@ -81,10 +81,9 @@ where
             let request = match Request::read_from(&mut buf, &mut client) {
                 Ok(request) => request,
                 Err(messages::Error::Json(err)) => {
-                    RequestResult::new_error(err)
-                        .response
-                        .write_to(&mut client)?;
-                    continue;
+                    return Response::new_error(err)
+                        .write_to(&mut client)
+                        .map_err(Into::into)
                 }
                 err => err?,
             };
@@ -97,21 +96,19 @@ where
             }
 
             let request_info = RequestInfo::new_now(request);
-            let result: RequestResult = {
-                if !handler.borrow().relevant(&request_info) {
-                    eprintln!("[INFO] Irrelevant request ({}): {:?}", addr, request_info);
-                    RequestResult::new_error("Irrelevant request")
-                } else {
-                    handler.borrow_mut().handle(request_info)?
-                }
-            };
+            if !handler.borrow().relevant(&request_info) {
+                eprintln!("[INFO] Irrelevant request ({}): {:?}", addr, request_info);
+                return Response::new_error("Irrelevant request")
+                    .write_to(&mut client)
+                    .map_err(Into::into);
+            }
 
-            eprintln!("{:?}", result.response);
             let RequestResult {
                 response,
                 new_handler,
-            } = result;
+            } = handler.borrow_mut().handle(request_info)?;
 
+            eprintln!("{:?}", response);
             response.write_to(&mut client)?;
 
             if let Some(new_handler) = new_handler {
